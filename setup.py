9c3b3ea5d7cb8cf87d90873d67abe3ff9db84eb5
Henrik Finsberg <henriknf@simula.no>
2020-12-13 10:46:43 +0100

    Add package dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = ["plotly", "fenics", "numpy"]
 
 setup(
     author="Henrik Finsberg",
