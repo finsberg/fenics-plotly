d72e645c8cd60503c73f90ad49ce4d5fc7d5e2ae
Henrik Finsberg <henriknf@simula.no>
2020-12-13 19:23:00 +0100

    Fix README on pypi

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="fenics_plotly",
     name="fenics_plotly",
